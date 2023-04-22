3482b1db9d5dfdd2bba9aaeb5deb9e27483da7a5
GreenTeethSkull <angelitorios366@gmail.com>
2023-04-22 10:16:11 -0500

    add display support to battery alert

--- battery.fish
+++ battery.fish
@@ -1,13 +1,14 @@
 #!/usr/bin/env fish
-
+set -x DISPLAY ":1"
 while true
     set -l battery_level (acpi -b | grep -P -o '[0-9]+(?=%)')
-    if test $battery_level -ge 91 
-        #notify-send "Battery is charge!" "Level: ${battery_level}%"
-        zenity --warning --title "Battery" --text "Battery is charge : $battery_level %"    
-    else if test $battery_level -le 21 
-        #notify-send "Battery is lower!" "Level: ${battery_level}%"
-        zenity --error --title "Battery" --text "Battery is lower : $battery_level %"     
+    set -l is_charging (acpi -b | grep -P -o '[a-zA-z]+(?=,)')
+    if test $battery_level -ge 91 -a $is_charging = 'Charging'
+        #notify-send "Battery is charge!" "Level: $battery_level%"
+        zenity --info --title "Battery" --text "Battery is charge : $battery_level %"    
+    else if test $battery_level -le 21 -a $is_charging = 'Discharging'
+        #notify-send "Battery is lower!" "Level: $battery_level%"
+        zenity --info --title "Battery" --text "Battery is lower : $battery_level %"     
     end
 
     sleep 240 # seconds
--- battery.sh
+++ battery.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+export DISPLAY=":1"
 while true
 do
     battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-    if [ $battery_level -ge 91 ]; then
-       #notify-send "Battery is charge!" "Level: ${battery_level}%"
-       zenity --warning --title "Battery" --text "Battery is charge : ${battery_level} %"    
-    elif [ $battery_level -le 21 ]; then
-       #notify-send "Battery is lower!" "Level: ${battery_level}%"
-       zenity --error --title "Battery" --text "Battery is lower : ${battery_level} %"     
+    is_charging=`acpi -b | grep -P -o '[a-zA-Z]+(?=,)'`
+    if ([ $battery_level -ge 91 ] && [[ $is_charging == 'Charging' ]]); then
+        #notify-send "Battery is charge!" "Level: ${battery_level}%"
+        zenity --warning --title "Battery" --text "Battery is charge : ${battery_level} %"    
+    elif ([ $battery_level -le 21 ] && [[ $is_charging == 'Discharging' ]]); then
+        #notify-send "Battery is lower!" "Level: ${battery_level}%"
+        zenity --error --title "Battery" --text "Battery is lower : ${battery_level} %"     
     fi
 
     sleep 240 # seconds
