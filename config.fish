96fb4a5d5374bc9b62e92c2ef984576f83fd2ded
GreeTeethSkull <angelitorios366@gmail.com>
2024-02-24 23:14:42 -0500

    change to vivaldi

--- config.fish
+++ config.fish
@@ -1,15 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    oh-my-posh init fish --config /home/greenskull/.poshthemes/greenskull_tux_min.omp.json | source
+    oh-my-posh init fish --config /home/greenskull/.poshthemes/greenskull_min.omp.json | source
     # Run local apps
     set -x PATH /home/greenskull/.local/bin $PATH
     set -x PATH /home/greenskull/bin $PATH
     set -x PATH /usr/local/bin/ $PATH
     # FLUTTER
     set -x PATH /opt/flutter/bin $PATH
-    set -x CHROME_EXECUTABLE /usr/bin/brave
+    set -x CHROME_EXECUTABLE /usr/bin/vivaldi
     # TERMINAL ICONS
-    alias ls=logo-ls
+    #alias ls=logo-ls
+    # SCALE APPS
+    set -x GDK_SCALE 1.25
     # HINT COLOR IN FISH
     set -g fish_color_autosuggestion "#5aa1db"
 end
