09052e7fa0cd0518e2402ece890e44e1669194fd
Angel Rios <angelitorios366@gmail.com>
2024-06-08 21:15:13 -0500

    8-6-2024

--- config.fish
+++ config.fish
@@ -6,14 +6,21 @@ if status is-interactive
     set -x PATH /home/greenskull/bin $PATH
     set -x PATH /usr/local/bin/ $PATH
     # FLUTTER
-    set -x PATH /opt/flutter/bin $PATH
+    set -x PATH /home/greenskull/.cache/flutter_sdk $PATH
     set -x CHROME_EXECUTABLE /usr/bin/vivaldi
     # TERMINAL ICONS
     #alias ls=logo-ls
     # SCALE APPS
     set -x GDK_SCALE 1.25
+    # GTK THEME
+    set -x GDK_THEME Whitesur-Dark-green
     # HINT COLOR IN FISH
     set -g fish_color_autosuggestion "#5aa1db"
+    # EXPO ANDROID
+    set -x ANDROID_HOME /home/greenskull/Android/Sdk
+    # ANDROID EMULATOR
+    set -x ANDROID_SDK_ROOT /home/greenskull/Android/Sdk   
 end
 
 set fish_greeting
+
